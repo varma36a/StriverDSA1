da46ddbb400d7eda3435e82786234f4b2c058b35
rohi <varma36a@gmail.com>
2025-02-01 14:10:57 +0530

    Initial Changes

---
+++ MyApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SetZeroes(); // Call the method
+    }
+
+    public static void SetZeroes()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
